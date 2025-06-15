5af3e641dcf67d2b47832dc1856c6ef83ac28b5b
DevSandeepX <sandeep87565590@gmail.com>
2025-06-15 18:07:36 +0530

    Fixe some bugs

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { setCredentials } from "../../features/auth/authSlice"
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500',
+    baseUrl: 'https://technotes-api.onrender.com',
     credentials: 'include',  // ⬅️ Required if refresh token is in cookie
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
